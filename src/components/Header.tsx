c506f83c09981f512575c25d7d028e5554788634
aldokusuma31 <aldo.kusumawardana@gmail.com>
2024-06-22 09:19:17 +0700

    main

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,11 +2,11 @@ import Link from 'next/link';
 
 const Header: React.FC = () => {
   return (
-    <header className="bg-black text-white py-4">
+    <header className="bg-gray-800 text-white py-4">
       <div className="flex max-w-7xl mx-auto px-4 justify-between items-center">
-        <h1 className="text-2xl font-bold">Dieng Adventure</h1>
+        <h1 className="text-xl font-bold">Dieng Adventure</h1>
         <nav>
-          <ul className="flex space-x-4">
+          <ul className="flex space-x-4 text-sm">
             <li>
               <Link href="/">Home</Link>
             </li>
